110f9671428f0708f973bf080bdf21122477ee40
Abhirup Majumder <59386224+Abhirup70@users.noreply.github.com>
2022-11-07 17:13:54 +0530

    Update main_file.m

--- main_file.m
+++ main_file.m
@@ -1,3 +1,12 @@
+%% Initialization
+clear ; close all; clc
+
+%% Setup the parameters you will use for this exercise
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          % (note that we have mapped "0" to 
+
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
